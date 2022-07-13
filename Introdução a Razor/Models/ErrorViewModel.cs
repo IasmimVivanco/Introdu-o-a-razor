b7fc929f7b7f8d751f4b27ba07af8a9e0a9ba66e
IasmimVivanco <iasmim.vivanco@hotmail.com>
2022-07-13 13:16:06 -0300

    Atividade de introdução a razor

---
+++ Introdução a Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Introdução_a_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
